b78a40d2dca2e525e0acf231e3157f2932b4d93e
Kanghee Park <kpark247@wisc.edu>
2023-12-03 14:45:46 -0600

    Implement string conversion for core language

--- src/main/scala/Lang.scala
+++ src/main/scala/Lang.scala
@@ -1,22 +1,61 @@
-sealed trait SecurityLabel
-case object Low extends SecurityLabel
-case object High extends SecurityLabel
+sealed trait SecurityLabel {
+  def toString: String
+}
+case object Low extends SecurityLabel {
+  override def toString: String = "L"
+}
+case object High extends SecurityLabel {
+  override def toString: String = "H"
+}
 
-sealed trait Type
-case object UnitType extends Type
-case class IntType(l: SecurityLabel) extends Type
-case class RefType(ty: Type, l: SecurityLabel) extends Type
+sealed trait Type {
+  def toString: String
+}
+case object UnitType extends Type {
+  override def toString: String = "Unit"
+}
+case class IntType(l: SecurityLabel) extends Type {
+  override def toString: String = s"Int(${l.toString})"
+}
+case class RefType(ty: Type, l: SecurityLabel) extends Type {
+  override def toString: String = s"${ty.toString} ref(${l.toString})"
+}
 
 class Lang {
-  sealed trait Expression
-  case class Variable(id: String) extends Expression
-  case class Num(n: Int) extends Expression
-  case object Unit extends Expression
-  case class Deref(e: Expression) extends Expression
-  case class Assign(e1: Expression, e2: Expression) extends Expression
-  case class Ref(e: Expression) extends Expression
-  case class Bind(v: Variable, e1: Expression, e2: Expression) extends Expression
-  case class Ite(cond: Expression, e1: Expression, e2: Expression) extends Expression
+  sealed trait Expression {
+    def toString: String
+  }
+
+  sealed trait Value extends Expression
+
+  case class Variable(id: String) extends Value {
+    override def toString: String = id
+  }
+  case class Num(n: Int) extends Value {
+    override def toString: String = n.toString
+  }
+  case object Unit extends Value {
+    override def toString: String = "()"
+  }
+  case class Deref(e: Value) extends Expression {
+    override def toString: String = s"!${e.toString}"
+  }
+  case class Assign(e1: Value, e2: Value) extends Expression {
+    override def toString: String = s"${e1.toString} := ${e2.toString}"
+  }
+  case class Ref(e: Value) extends Expression {
+    override def toString: String = s"ref ${e.toString}"
+  }
+  case class Bind(x: Variable, e1: Expression, e2: Expression) extends Expression {
+    override def toString: String =
+      s"""bind ${x.toString} = ${e1.toString} in
+         |${e2.toString}
+         |""".stripMargin
+  }
+  case class Ite(cond: Value, e1: Expression, e2: Expression) extends Expression {
+    override def toString: String =
+      s"if ${cond.toString} then ${e1.toString} else ${e2.toString}"
+  }
 }
 
 // Language for bidirectional type checking (for type-guided synthesis)
