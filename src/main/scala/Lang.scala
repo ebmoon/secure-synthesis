e4d7ab9fb5ec18196ceeb01eab42c8f91f338f80
Kanghee Park <kpark247@wisc.edu>
2023-12-06 13:01:41 -0600

    Update bidirectional language definition

--- src/main/scala/Lang.scala
+++ src/main/scala/Lang.scala
@@ -1,70 +1,95 @@
 object Lang {
   sealed trait Expression {
     def toString: String
-    def size: Int
   }
 
   sealed trait Value extends Expression
 
   case class Variable(id: String) extends Value {
     override val toString: String = id
-    override val size = 1
   }
   case object True extends Value {
     override val toString: String = "true"
-    override val size = 1
   }
   case object False extends Value {
-    override val toString: String = "true"
-    override val size = 1
+    override val toString: String = "false"
   }
   case class Num(n: Int) extends Value {
     override val toString: String = n.toString
-    override val size = 1
   }
   case object Unit extends Value {
     override val toString: String = "()"
-    override val size = 1
   }
   case class Deref(e: Variable) extends Expression {
     override def toString: String = s"!${e.toString}"
-    override val size = e.size + 1
   }
   case class Assign(e1: Variable, e2: Value) extends Expression {
     override def toString: String = s"${e1.toString} := ${e2.toString}"
-    override val size = e1.size + e2.size + 1
   }
   case class Ref(e: Value) extends Expression {
     override def toString: String = s"ref ${e.toString}"
-    override val size = e.size + 1
   }
   case class Bind(x: Variable, e1: Expression, e2: Expression) extends Expression {
     override def toString: String =
       s"""bind ${x.toString} = ${e1.toString} in
          |${e2.toString}
          |""".stripMargin
-    override val size = e1.size + e2.size + 1
   }
   case class Ite(cond: Value, e1: Expression, e2: Expression) extends Expression {
     override def toString: String =
       s"if ${cond.toString} then ${e1.toString} else ${e2.toString}"
-    override val size = cond.size + e1.size + e2.size + 1
   }
 }
 
-// Language for bidirectional type checking (for type-guided synthesis)
-// Not used in naive enumerative synthesizer
 object BidirectionalLang {
-  sealed trait ElimExpression
-  case class Variable(id: String) extends ElimExpression
-  case class Num(n: Int) extends ElimExpression
-  case object Unit extends ElimExpression
-  case class Deref(e: ElimExpression) extends ElimExpression
-  case class Assign(e1: ElimExpression, e2: IntroExpression) extends ElimExpression
+  sealed trait Expression {
+    def toString: String
+  }
 
-  sealed trait IntroExpression
-  case class ElimToIntro(e: ElimExpression) extends IntroExpression
-  case class Ref(e: IntroExpression) extends IntroExpression
-  case class Bind(v: Variable, e1: ElimExpression, e2: IntroExpression) extends IntroExpression
-  case class Ite(cond: ElimExpression, e1: IntroExpression, e2: IntroExpression) extends IntroExpression
+  sealed trait ElimExpression extends Expression
+  case class Deref(e: ElimValue) extends ElimExpression {
+    override def toString: String = s"!${e.toString}"
+  }
+  case class Assign(e1: Variable, e2: IntroValue) extends ElimExpression {
+    override def toString: String = s"${e1.toString} := ${e2.toString}"
+  }
+
+  // Not a value in the manner of evaluation, used for value restriction
+  sealed trait ElimValue extends ElimExpression
+  case class Variable(id: String) extends ElimValue {
+    override val toString: String = id
+  }
+  case object True extends ElimValue {
+    override val toString: String = "true"
+  }
+  case class Num(n: Int) extends ElimValue {
+    override val toString: String = "false"
+  }
+  case object Unit extends ElimValue {
+    override val toString: String = "())"
+  }
+
+  sealed trait IntroExpression extends Expression
+  case class ElimToIntro(e: ElimExpression) extends IntroExpression {
+    override def toString: String = e.toString
+  }
+  case class Ref(e: IntroValue) extends IntroExpression {
+    override def toString: String = s"ref ${e.toString}"
+  }
+  case class Ite(cond: ElimExpression, e1: IntroExpression, e2: IntroExpression) extends IntroExpression {
+    override def toString: String =
+      s"if ${cond.toString} then ${e1.toString} else ${e2.toString}"
+  }
+
+  sealed trait IntroValue extends IntroExpression
+  case class ElimValueToIntroValue(e: ElimValue) extends IntroValue {
+    override def toString: String = e.toString
+  }
+
+  case class Bind(v: Variable, e1: ElimExpression, e2: Expression) extends Expression {
+    override def toString: String =
+      s"""bind ${x.toString} = ${e1.toString} in
+         |${e2.toString}
+         |""".stripMargin
+  }
 }
--- src/main/scala/Type.scala
+++ src/main/scala/Type.scala
@@ -1,11 +1,44 @@
 sealed trait SecurityLabel {
   def toString: String
+  def join(other: SecurityLabel): SecurityLabel
 }
 case object Low extends SecurityLabel {
   override def toString: String = "L"
+  override def join(other: SecurityLabel): SecurityLabel = other
 }
 case object High extends SecurityLabel {
   override def toString: String = "H"
+  override def join(other: SecurityLabel): SecurityLabel = this
+}
+// Temporary security labels used for type inference
+case class SecurityVar(id: String) extends SecurityLabel {
+  override def toString: String = id
+  override def join(other: SecurityLabel): SecurityLabel = {
+    other match {
+      case Low => this
+      case High => other
+      case _ => Join(this, other)
+    }
+  }
+}
+case class Join(l1: SecurityLabel, l2: SecurityLabel) extends SecurityLabel {
+  override def toString: String = s"${l1.toString} |_| ${l2.toString}"
+  override def join(other: SecurityLabel): SecurityLabel = {
+    other match {
+      case Low => this
+      case High => other
+      case _ => Join(this, other)
+    }
+  }
+}
+
+object SecurityLabel {
+  var counter: Int = 0
+  def freshVar: SecurityVar = {
+    val id = s"l${counter}"
+    counter = counter + 1
+    SecurityVar(id)
+  }
 }
 
 sealed trait Type {
